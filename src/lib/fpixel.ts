769d1121579621818ad194015c8f118cf24fbe62
Gannon Gesiriech <ggesiriech@gmail.com>
2025-05-09 16:25:20 -0700

    fix any

--- src/lib/fpixel.ts
+++ src/lib/fpixel.ts
@@ -4,14 +4,17 @@ export const pageview = () => {
   window.fbq('track', 'PageView')
 }
 
+// Define a type for the Facebook Pixel event options
+type FacebookPixelEventOptions = Record<string, string | number | boolean>;
+
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
-export const event = (name: string, options = {}) => {
+export const event = (name: string, options: FacebookPixelEventOptions = {}) => {
   window.fbq('track', name, options)
 }
 
 // Typescript type definitions
 declare global {
   interface Window {
-    fbq: (type: string, eventName: string, options?: any) => void
+    fbq: (type: string, eventName: string, options?: FacebookPixelEventOptions) => void
   }
 } 
