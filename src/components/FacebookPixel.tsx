da69dd234ed9e127b70018794f26a5f9db728153
Gannon Gesiriech <ggesiriech@gmail.com>
2025-05-09 15:30:53 -0700

    troubleshoot react comp 1

--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -1,1 +1,21 @@
- 
+'use client';
+
+import { useEffect } from 'react';
+import ReactPixel from 'react-facebook-pixel';
+
+const FacebookPixel = (): null => {
+  useEffect(() => {
+    // Initialize the Facebook Pixel
+    ReactPixel.init('1706068603634026', undefined, {
+      autoConfig: true,
+      debug: false,
+    });
+
+    // Track page view
+    ReactPixel.pageView();
+  }, []);
+
+  return null;
+};
+
+export default FacebookPixel; 
