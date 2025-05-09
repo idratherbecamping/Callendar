628f38d9feaceb734d9bd9bcb269ee2332c42661
Gannon Gesiriech <ggesiriech@gmail.com>
2025-05-09 15:45:59 -0700

    utilize basic approach

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter, Poppins, Lexend_Deca } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import ClientBody from "./ClientBody";
-import FacebookPixel from "@/components/FacebookPixel";
-import PageViewTracker from "@/components/PageViewTracker";
+import { trackPageView } from "@/lib/analytics";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -37,14 +37,37 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Track page view on client side
+  if (typeof window !== 'undefined') {
+    trackPageView(window.location.pathname);
+  }
+
   return (
     <html lang="en" className={`${inter.variable} ${poppins.variable} ${lexendDeca.variable}`}>
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1706068603634026');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript dangerouslySetInnerHTML={{
+        __html: `<img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1706068603634026&ev=PageView&noscript=1"
+        />`
+      }} />
       <body className={`${inter.variable} ${poppins.variable} ${lexendDeca.variable}`}>
         <ClientBody>
           <div className="dark">{children}</div>
           <Analytics />
-          <FacebookPixel />
-          <PageViewTracker />
         </ClientBody>
       </body>
     </html>
